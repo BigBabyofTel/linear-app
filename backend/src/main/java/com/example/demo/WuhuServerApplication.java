710f429717bf488c5062ff4b5a484dc30f9cd420
nixowl <gabriellamondvay@gmail.com>
2024-04-17 16:26:45 +0200

    add backend folder

---
+++ backend/src/main/java/com/example/demo/WuhuServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuhuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuhuServerApplication.class, args);
+	}
+
+}
