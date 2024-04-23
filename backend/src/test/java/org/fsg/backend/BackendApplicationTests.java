9d72ac630838fca8d3ec04d2aa514f60b6cc79c0
Luka <lukabrkovic@proton.me>
2024-04-23 18:06:02 +0200

    backend init

---
+++ backend/src/test/java/org/fsg/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fsg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
