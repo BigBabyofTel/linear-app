2a24b1de8529185a9a7ff3697c81d1943abf9e2b
Luka <lukabrkovic@proton.me>
2024-05-01 16:54:06 +0200

    feat: sign up sends user to temp

--- frontend/src/app/(signup-signin)/sign-in/sign-in-form.tsx
+++ frontend/src/app/(signup-signin)/sign-in/sign-in-form.tsx
@@ -4,11 +4,10 @@ import { Card } from "@/components/ui/card";
 import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { signInAction } from "@/lib/actions";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function SignInForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
     <Card className="mx-auto grid gap-6 rounded-md p-0 shadow-sm sm:w-[30rem] sm:border sm:border-border sm:bg-card md:w-[35rem]">
       <div>
@@ -17,29 +16,30 @@ export function SignInForm() {
           <p className="block text-sm text-muted-foreground sm:hidden">Embrace the future of work.</p>
           <p className="hidden text-sm text-muted-foreground sm:block">Embrace the future of work. Sign up now.</p>
         </div>
-        <div className="grid gap-2 pb-8 pt-4 text-left sm:px-8">
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            const email = formData.get("email") as string;
+            const password = formData.get("password") as string;
+            const res = await signInAction({ email, password });
+
+            if (res.data?.success) {
+              router.push("/temp");
+            }
+          }}
+          className="grid gap-2 pb-8 pt-4 text-left sm:px-8"
+        >
           <Field label="Email" labelFor="email" error={""}>
-            <Input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id="email"
-              type="email"
-              placeholder="Your Email"
-            />
+            <Input name="email" id="email" type="email" placeholder="Your Email" />
           </Field>
           <Field label="Password" labelFor="password" error={""}>
-            <Input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              id="password"
-              type="password"
-              placeholder="Create a Password"
-            />
+            <Input name="password" id="password" type="password" placeholder="Create a Password" />
           </Field>
-          <Button onClick={async () => signInAction({ email, password })} className="mt-4 w-full">
+          <Button type="submit" className="mt-4 w-full">
             Sign Up
           </Button>
-        </div>
+        </form>
       </div>
     </Card>
   );
