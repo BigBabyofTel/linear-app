e21bd7b65af30872799622786c4f99dbbed0add2
nixowl <gabriellamondvay@gmail.com>
2024-05-02 15:21:14 +0200

    small fixes to sidebar styling, add button to open create issue modal

--- frontend/src/components/layout/dashboard/sidebar.tsx
+++ frontend/src/components/layout/dashboard/sidebar.tsx
@@ -1,17 +1,26 @@
 "use client";
+import { CreateIssue } from "@/components/forms/create-issue";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { PenBoxIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Sidebar = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex h-screen w-[23%] flex-col items-center rounded-2xl border-8 p-3 shadow-inner shadow-white">
-      <h1 className="p-3 text-3xl">Wuhu</h1>
-      <section className="m-2 border-8">
-        <ThemeToggle />
-      </section>
-      <section className="fixed top-[73%] flex h-60 w-[16%] flex-col justify-evenly">
+    <div className="flex h-screen w-[23%] flex-col items-center gap-2 rounded-2xl border p-3 min-w-[260px]">
+      <div className="flex flex-row justify-between w-[80%]">
+          <select name="workspace">Workspace selector
+          <option value="1">Workspace 1</option>
+          <option value="2">Workspace 2</option>
+        </select>
+        <Button className="py-2 px-1" variant="secondary" onClick={() => setIsOpen(true)}><PenBoxIcon /></Button>
+        <CreateIssue isOpen={isOpen} closeFn={() => setIsOpen(false)} />
+      </div>
+      <ThemeToggle />
+      <section className="flex flex-col gap-1">
         <Button className="rounded" variant="blurry" onClick={() => router.push("/dashboard/workplace")}>
           Workplace
         </Button>
