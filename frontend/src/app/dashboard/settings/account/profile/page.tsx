a6d991d4b623c00f335e25dce7efa3298306684e
BigBabyofTel <augustus.tb@outlook.com>
2024-05-02 22:41:23 +0300

    token added to all requests/profile updated with modal

--- frontend/src/app/dashboard/settings/account/profile/page.tsx
+++ frontend/src/app/dashboard/settings/account/profile/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+
+import DeleteAccount from "@/components/settings/deleteaccount";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { changePassword } from "@/lib/actions";
-import { Avatar, AvatarIcon } from "@nextui-org/avatar";
-import { CalendarHeart } from "lucide-react";
-import { Form } from "react-hook-form";
+import { useState } from "react";
+
+
 
 export default function Page() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <main className=" align-items-center m-2 flex flex-col p-5">
@@ -13,13 +18,13 @@ export default function Page() {
           <h1 className="flex justify-start text-left text-3xl">Profile</h1>
           <p className="text-sm text-muted-foreground">Manage your profile</p>
         </section>
-        <section className="m-5 flex justify-center flex-col items-center">
-          <Card className="w-[600px] bg-card-background m-4">
+        <section className="m-5 flex flex-col items-center justify-center">
+          <Card className="bg-card-background m-4 w-[600px]">
             <CardHeader className=" bg-">
               <div className="">
-                <h2 className="p-2 mb-5">Profile picture</h2>
+                <h2 className="mb-5 p-2">Profile picture</h2>
               </div>
-              <div className="flex h-[200px] w-[200px] items-center justify-center rounded-full border-4 border-slate-600 text-center mx-auto" >
+              <div className="mx-auto flex h-[200px] w-[200px] items-center justify-center rounded-full border-4 border-slate-600 text-center">
                 <p className="text-5xl">T.A.B.</p>
               </div>
             </CardHeader>
@@ -40,22 +45,28 @@ export default function Page() {
               </section>
             </CardContent>
           </Card>
-          <Card className="w-[600px]">
+          <Card className="w-[600px] m-4">
             <CardHeader className="text-center">Change Password</CardHeader>
             <CardContent>
               <form action={changePassword}>
-              <section className="flex justify-between p-5">
-              <label htmlFor="current password">Current Password</label>
-              <input type="password" name="currentPassword" />
-              </section>
-              <section className="flex justify-between p-5">
-              <label htmlFor="new password">New Password</label>
-              <input type="password" name="newPassword" />
-              </section>
-              <section className="flex justify-center p-5">
-              <Button>Change</Button>
-              </section>
+                <section className="flex justify-between p-5">
+                  <label htmlFor="current password">Current Password</label>
+                  <input type="password" name="currentPassword" />
+                </section>
+                <section className="flex justify-between p-5">
+                  <label htmlFor="new password">New Password</label>
+                  <input type="password" name="newPassword" />
+                </section>
+                <section className="flex justify-center p-5">
+                  <Button>Change</Button>
+                </section>
               </form>
+            </CardContent>
+          </Card>
+          <Card className="w-[600px] m-4 flex flex-col items-center">
+            <CardHeader>Delete Account</CardHeader>
+            <CardContent>
+              <Button onClick={() => setOpen(true)}><DeleteAccount isOpen={open} closeFn={() => setOpen(false)} />Delete</Button>
             </CardContent>
           </Card>
         </section>
